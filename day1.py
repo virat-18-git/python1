1d49ae7dfc9b9b29f244f8a52e8fd3236b7bb477
virat-18-git <ksprddy6@gmail.com>
2024-12-23 21:19:44 +0530

    1

---
+++ day1.py
@@ -0,0 +1,13 @@
+# num1 = int(input("enter the number to be printed 100"))
+# num2 = int(input("enter number 50"))
+# print(num1)
+# print(num2)
+# print(num1+num2)
+
+cmplx1=5+11j
+cmplx2=6+8j
+print(cmplx1+cmplx2)
+print(cmplx1-cmplx2)
+print(cmplx1*cmplx2)
+print(cmplx1/cmplx2)
+print(cmplx1%cmplx2)
